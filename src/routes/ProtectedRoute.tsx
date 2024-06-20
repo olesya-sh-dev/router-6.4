43f17ef9597733c46dca9e21a8c90c67aa8b34b6
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-06-20 14:09:18 +0300

    privateRoutes/publicRoutes

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react'
 import { Navigate } from 'react-router-dom'
-import { ProtectedPage } from '../components/pages/ProtectedPage'
+
 
 type Props = {
     children: ReactNode
@@ -8,7 +8,7 @@ type Props = {
 
 export const ProtectedRoute = ({children}: Props) => {
 const logged = false
-  return logged ? <ProtectedPage/> : <Navigate to={'/error'}/>
+  return logged ? children : <Navigate to={'/error'}/>
 
 }
 
